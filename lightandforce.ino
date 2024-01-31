45dc0385b2ebec1835531e6ecdd8f45944475891
Peter Riemersma <35268498+GroteGehaktBal@users.noreply.github.com>
2024-01-31 15:48:02 +0100

    added profiles for demo

--- lightandforce.ino
+++ lightandforce.ino
@@ -1,11 +1,12 @@
 // code voor de esp met de daglichtsensor, force sensor en de verlichting
 
-#define LIGHT_SENSOR_PIN  25  // ESP32 pin GPIO36 (ADC0) connected to light sensor
-#define LED_PIN           33  // ESP32 pin GPIO22 connected to LED
+#define LIGHT_SENSOR_PIN  25
+#define LED_PIN           33
 #define ANALOG_THRESHOLD  500
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT); // set ESP32 pin to output mode
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -15,4 +16,22 @@ void loop() {
     digitalWrite(LED_PIN, HIGH); // turn on LED
   else
     digitalWrite(LED_PIN, LOW);  // turn off LED
+
+  Serial.print("Analog Value = ");
+  Serial.print(analogValue);
+
+// Profiles
+  if (analogValue < 100) {
+    Serial.println(" => Dark");
+  } else if (analogValue < 800) {
+    Serial.println(" => Dim");
+  } else if (analogValue < 2000) {
+    Serial.println(" => Light");
+  } else if (analogValue < 3200) {
+    Serial.println(" => Bright");
+  } else {
+    Serial.println(" => Very bright");
+  }
+
+  delay(500);
 }
